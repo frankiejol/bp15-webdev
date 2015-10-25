4170c94cd894f933a1620aeafb6062ee9a76e767
Francesc Guasch <frankie@etsetb.upc.edu>
2015-10-25 11:55:44 +0100

    Base code

---
+++ lib/BookZone/Controller/Example.pm
@@ -0,0 +1,12 @@
+package BookZone::Controller::Example;
+use Mojo::Base 'Mojolicious::Controller';
+
+# This action will render a template
+sub welcome {
+  my $self = shift;
+
+  # Render template "example/welcome.html.ep" with message
+  $self->render(data => '<h1>Welcome !</h1>');
+}
+
+1;
